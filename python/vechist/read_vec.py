674bff011c76084535e86c05b4ac3fda6d0bf7e2
Chris Jacobsen <datahead8888@gmail.com>
2015-04-21 13:17:07 -0400

    Got a tree structure building for each parsed tree in Python

--- python/vechist/read_vec.py
+++ python/vechist/read_vec.py
@@ -5,6 +5,7 @@ Created on Tue Nov 11 17:38:33 2014
 @author: tong
 """
 import re
+#import MergeTreeNode
 
 def read(filename):
 #filename = "D:/data/plume/15plume3d421.vec"
@@ -40,11 +41,26 @@ def read(filename):
 
     return d
 
+#Definition for a tree node with a list of children - used for the merge tree
+class MergeTreeNode:
+    def __init__(self):
+        self.haloId = -1
+        self.children = []
+
+    def SetHaloId(self, newId):
+        self.haloId = newId
+
+
+
+
+
 #This routine will read the merger tree file at the specified location and load it into a basic data structure
 def readMergerTree(filename):
     f = open(filename, "r")
 
-    treeId = 0 #Default value
+    treeId = 0 #Default value for treeId
+    dictionary = {} #Empty dictionary to hold references to the merger tree nodes
+    root = None
 
     for line in f:
         #Ignore Comments
@@ -53,8 +69,14 @@ def readMergerTree(filename):
                 data_int_list = [str(record) for record in line.split()]
                 treeId = data_int_list[1]
                 print("New tree record, id: " + treeId)
+                dictionary.clear() #Wipe out the dictionary - we only use each dictionary once for each tree
+                #if root != None:
+                #    print("--------Printing current merge tree---------")
+                #    printMergerTree(root, 0)
+                #    print("--------Finished printing merge tree---------")
+                root = None
             continue
-        #Test for the entry for the number of trees in the forest
+        #Test the entry for the number of trees in the forest
         match = re.match(r"^[0-9]+ *$", line)
         if match:
             numTrees = int(line)
@@ -72,6 +94,45 @@ def readMergerTree(filename):
             descendant_id = int(float(data_int_list[3]))
             print("Descendant id: " + str(descendant_id))
             print("\n")
+
+            #Build a new tree node
+            treeNode = MergeTreeNode()
+            treeNode.SetHaloId(haloId)
+
+            #Set the root node if needed
+            if len(dictionary) == 0:
+                root = treeNode
+
+            #Store a reference to the treeNode in the dictionary
+            dictionary[haloId] = treeNode
+
+            #Look for the descendant and then link the new node to it in the tree
+            if descendant_id in dictionary:
+                parentNode = dictionary[descendant_id]
+                parentNode.children.append(treeNode)
+            elif descendant_id == -1:
+                print("Descendant id was -1 - ignoring")
+            else:
+                print("WARNING!!!!!!!   Descendant id " + str(descendant_id) + " does not have a halo entry already in the dictionary!!!!!!")
+
+#Uitility function to print the contents of the merge tree
+#It currently only prints through 4 depth levels (starting from 0)
+def printMergerTree(node, level):
+    print("New Node from tree:")
+    print("Halo id: " + str(node.haloId))
+    print("Tree level: " + str(level))
+    for childNode in node.children:
+        if level <= 2:
+            printMergerTree(childNode, level + 1)
+
+
+
+
+
+
+
+
+
 
 
 
