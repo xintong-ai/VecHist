13906935b8b4780a831865cc52cfd069139d9836
Chris Jacobsen <datahead8888@gmail.com>
2015-05-13 02:24:03 -0400

    Wrote main code for loading graph data into data structures

--- GraphVizWidget.cpp
+++ GraphVizWidget.cpp
@@ -2,6 +2,15 @@
 
 #include "GraphVizWidget.h"
 
+//Destructor
+GraphVizWidget::~GraphVizWidget()
+{
+	for (int i = 0; i < nodes.size(); i++) {
+		delete nodes[i];
+	}
+
+}
+
 
 void GraphVizWidget::loadGraphVizTextFile()
 {
@@ -19,15 +28,15 @@ void GraphVizWidget::loadGraphVizTextFile()
 	//edge tail head n x1 y1 ..xn yn[label xl yl] style color
 	//stop
 
-	double graphScale, graphWidth, graphHeight;  //Attributes about entire graph that will be needed to establish the coordinate system
-
 	string token; //The general token currently read from the file needing to be processed
 	string name; //The name of the node currently being processed
 	string headName; //The name of the head element of an edge currently being processed
 	string tailName; //The name of the tail element of an edge currently being processed
 	int numNodes;    //The number of nodes constituting one b-spline
 	double x, y; //The x and y coordinates of the node/edge being read
-	double width, height;  //The width and heigh of the node currently being read
+	double width, height;  //The width and height of the node currently being read
+
+	nodes.clear();
 
 	while (!inFile.fail()) {
 		inFile >> token;
@@ -39,6 +48,7 @@ void GraphVizWidget::loadGraphVizTextFile()
 			cout << "Scale: " << graphScale << " Width: " << graphWidth << " Height: " << graphHeight << endl;
 		}
 		else if (token == "node") {
+			//Parse the Node Record
 			cout << "New node record: " << endl;
 			inFile >> name;
 			cout << "Name: " << name << endl;
@@ -52,8 +62,23 @@ void GraphVizWidget::loadGraphVizTextFile()
 				cout << token << " ";
 			}
 			cout << endl;
+
+			//Store the node record
+			GraphVizNode * node = new GraphVizNode;
+			node -> name = name;
+			node -> x = x;
+			node -> y = y;
+			node -> width = width;
+			node -> height = height;
+
+			nodes.push_back(node);
+
+			nodeTable[name] = node;
+
+
 		}
 		else if (token == "edge") {
+			//////////////Parse the main part of the edge record//////////////////
 			cout << "New edge record:" << endl;
 			inFile >> headName;
 			cout << "Head Name: " << headName << endl;
@@ -61,18 +86,49 @@ void GraphVizWidget::loadGraphVizTextFile()
 			cout << "Tail Name: " << tailName << endl;
 			inFile >> numNodes;
 			cout << "Num Nodes: " << numNodes << endl;
+			
+			///////////Store the edge record///////////////////////
+			GraphVizNode * headPtr = nodeTable[headName];
+			if (headPtr == nullptr) {
+				cerr << headName << " not found!!!!!" << endl;
+				break;
+			}
+
+			GraphVizNode * tailPtr = nodeTable[tailName];
+			if (tailPtr == nullptr) {
+				cerr << tailName << " not found!!!!!" << endl;
+				break;
+			}
+
+			GraphVizEdge * edge = new GraphVizEdge();
+			edge->head = headPtr;
+			edge->tail = tailPtr;
+			edge->controlPoints.resize(numNodes);
+			
+
+			//////////Parse and store the control point records////
 			for (int i = 0; i < numNodes; i++) {
 				inFile >> x;
 				inFile >> y;
 				cout << "Control Point # " << i << ": " << x << " " << y << endl;
+
+				ControlPoint controlPoint;
+				controlPoint.x = x;
+				controlPoint.y = y;
+				edge->controlPoints.push_back(controlPoint);
 			}
-			cout << endl;
+
+			//Add the new edge to the list
+			edges.push_back(edge);
+
 			cout << "Unused tokens: " << endl;
 			for (int i = 0; i < 2; i++) {
 				inFile >> token;
 				cout << token << " ";
 			}
 			cout << endl;
+
+
 		}
 		else if (token == "stop") {
 			cout << "Stop request encountered" << endl;
--- GraphVizWidget.h
+++ GraphVizWidget.h
@@ -3,14 +3,44 @@
 
 #include <QWidget>
 #include <fstream>
+#include <unordered_map>
+#include <vector>
 
 using namespace std;
+
+struct GraphVizNode
+{
+	string name;
+	double x;  //X coordinate
+	double y;  //U coordinate
+	double width; //Ellipse width
+	double height; //Ellipse height
+};
+
+struct ControlPoint
+{
+	double x;
+	double y;
+};
+
+struct GraphVizEdge
+{
+	GraphVizNode * head; //Pointer to GraphVizNode representing the head
+	GraphVizNode * tail; //Pointer to GraphVizNode representing the head
+	vector<ControlPoint> controlPoints; //Set of control points representing the b-spline for the edge's curve
+
+};
 
 class GraphVizWidget : public QWidget
 {
 private:
-	ifstream inFile;
+	ifstream inFile;							 //Output file from dot to be read
+	double graphScale, graphWidth, graphHeight;  //Attributes about entire graph that will be needed to establish the coordinate system
+	unordered_map<string, GraphVizNode *> nodeTable; //HashTable linking node ids to the struct records
+	vector<GraphVizNode *> nodes;					//The list of all nodes
+	vector<GraphVizEdge *> edges;					//The list of all edges
 public:
+	~GraphVizWidget();
 	void loadGraphVizTextFile();
 
 };
